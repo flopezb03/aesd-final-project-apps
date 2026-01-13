67cbd8b87783db6b167b0519426a136ead059eeb
flopezb03 <f.lopezberrocal@gmail.com>
2026-01-13 13:37:19 +0100

    Refactoring unix_server_fd, unix_client_fd now are local main variables. functions modified for compatibility

--- device-client/main.c
+++ device-client/main.c
@@ -17,23 +17,24 @@ struct measures_data_t{
     time_t timestamp;
 };
 
-static void closeall();
+static void closeall(int unix_server_fd, int unix_client_fd);
 static void set_signals();
 static void term_handler(int);
 static void error_message(char* msg);
-static void init_server_unix_socket();
-static void accept_unix_client();
-static int read_from_client(struct measures_data_t* md);
+static int init_server_unix_socket(int* unix_server_fd, int* unix_client_fd);
+static int accept_unix_client(int unix_server_fd, int* unix_client_fd);
+static int read_from_client(int unix_client_fd, struct measures_data_t* md);
 
 
 static daemon_mode = 0;
 static end = 0;
-static int unix_server_fd = -1;
-static int unix_client_fd = -1;
+
 
 
 
 int main(int argc, char** argv){
+    int unix_server_fd = -1;
+    int unix_client_fd = -1;
     struct measures_data_t measures_data;
 
     if (argc == 2 && strcmp(argv[1], "-d") == 0){
@@ -44,17 +45,25 @@ int main(int argc, char** argv){
     set_signals();
 
     //  Init Unix Socket as Server
-    init_server_unix_socket();
+    if(!init_server_unix_socket(&unix_server_fd, &unix_client_fd)){
+        closeall(unix_server_fd, unix_client_fd);
+        exit(EXIT_FAILURE);
+    }
 
     while(!end){
 
         //  Read from client
-        if(!read_from_client(&measures_data)){
+        if(!read_from_client(unix_client_fd, &measures_data)){
             if(end)
                 continue;
+
             error_message("Client disconnected");
             close(unix_client_fd);
-            accept_unix_client();
+
+            if(!accept_unix_client(unix_server_fd, &unix_client_fd)){
+                closeall(unix_server_fd, unix_client_fd);
+                exit(EXIT_FAILURE);
+            }
             continue;
         }
 
@@ -64,11 +73,11 @@ int main(int argc, char** argv){
         
     }
 
-    closeall();
+    closeall(unix_server_fd, unix_client_fd);
     exit(EXIT_SUCCESS);
 }
 
-static void closeall(){
+static void closeall(int unix_server_fd, int unix_client_fd){
     if(unix_client_fd != -1)
         close(unix_client_fd);
     if(unix_server_fd != -1)
@@ -98,41 +107,42 @@ static void error_message(char* msg){
 
 
 
-static void init_server_unix_socket(){
+static int init_server_unix_socket(int* unix_server_fd, int* unix_client_fd){
     struct sockaddr_un unix_server_addr;
 
-    if((unix_server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
+    if((*unix_server_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1){
         error_message("Unix Socket Creation");
-        closeall();
-        exit(EXIT_FAILURE);
+        return 0;
     }
 
     unix_server_addr.sun_family = AF_UNIX;
     strncpy(unix_server_addr.sun_path, UNIX_SOCKET_NAME, sizeof(unix_server_addr.sun_path) - 1);
 
-    if(bind(unix_server_fd, (const struct sockaddr*) &unix_server_addr, sizeof(unix_server_addr)) == -1){
+    if(bind(*unix_server_fd, (const struct sockaddr*) &unix_server_addr, sizeof(unix_server_addr)) == -1){
         error_message("Bind Unix Server");
-        closeall();
-        exit(EXIT_FAILURE);
+        return 0;
     }
 
-    if(listen(unix_server_fd, 1) == -1){
+    if(listen(*unix_server_fd, 1) == -1){
         error_message("Listen Unix Server");
-        closeall();
-        exit(EXIT_FAILURE);
+        return 0;
     }
     
-    accept_unix_client();
+    if(!accept_unix_client(*unix_server_fd, unix_client_fd))
+        return 0;
+
+    return 1;
 }
-static void accept_unix_client(){
-    if((unix_client_fd = accept(unix_server_fd, NULL, NULL)) == -1){
+static int accept_unix_client(int unix_server_fd, int* unix_client_fd){
+    if((*unix_client_fd = accept(unix_server_fd, NULL, NULL)) == -1){
         error_message("Accept Unix Server");
-        closeall();
-        exit(EXIT_FAILURE);
+        return 0;
     }
+
+    return 1;
 }
 
-static int read_from_client(struct measures_data_t* md){
+static int read_from_client(int unix_client_fd, struct measures_data_t* md){
     return recv(unix_client_fd, md, sizeof(struct measures_data_t), 0) > 0;
 }
 
